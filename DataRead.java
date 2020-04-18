be10befebc979727225d8fb2304e307a153383b0
Schusswaffel <wissidi8@gmail.com>
2020-04-19 00:04:03 +0200

    stuff

--- DataRead.java
+++ DataRead.java
@@ -8,38 +8,38 @@ import java.util.Scanner;
 public class DataRead {
 
     public static void main(String[] args) throws IOException {
-    File file = new File("C:/temp/UserData.txt");
-    File sendFile = new File("C:/temp/UserDataSend.txt");
-    PrintWriter pWriter = null;
-    pWriter = new PrintWriter(new FileWriter(sendFile));
-    
-    Scanner scanner = new Scanner(file);
-    ArrayList<Sentence> sentences = new ArrayList<>();
+        File file = new File("C:/temp/UserData.txt");
+        File sendFile = new File("C:/temp/UserDataSend.txt");
+        PrintWriter pWriter = null;
+        pWriter = new PrintWriter(new FileWriter(sendFile));
+        
+        Scanner scanner = new Scanner(file);
+        ArrayList<Sentence> sentences = new ArrayList<>();
         String name;
-    while (scanner.hasNextLine())
-    {
-        String line = scanner.nextLine();
-        String[]tokens = line.split(";");
-         name = tokens[0];
-        String verb = tokens[1];
-        String adjectiv = tokens[2];
-        Sentence s =new Sentence(name,verb, adjectiv);
-        sentences.add(s);
-        System.out.println( sentences.get(0).getString(name));
-        pWriter.write(name+";");
+        while (scanner.hasNextLine())
+        {
+            String line = scanner.nextLine();
+            String[]tokens = line.split(";");
+            name = tokens[0];
+            String verb = tokens[1];
+            String adjectiv = tokens[2];
+            Sentence s =new Sentence(name,verb, adjectiv);
+            sentences.add(s);
+            System.out.println( sentences.get(0).getString(name));
+            pWriter.write(name+";");
 
-        System.out.println( sentences.get(0).getString(verb));
-        pWriter.write(verb+";");
+            System.out.println( sentences.get(0).getString(verb));
+            pWriter.write(verb+";");
 
-        System.out.println( sentences.get(0).getString(adjectiv));
-        pWriter.write(adjectiv+";\n");
+            System.out.println( sentences.get(0).getString(adjectiv));
+            pWriter.write(adjectiv+";\n");
+        }
+        
+        scanner.close();
+        int j = sentences.size();
+        System.out.println(j);
+        String g = "Hallo";
+        g.length();
+        pWriter.close();
     }
-    
-    scanner.close();
-     int j = sentences.size();
-     System.out.println(j);
-    String g = "Hallo";
-    g.length();
-    pWriter.close();
-}
 }
--- UI.java
+++ UI.java
@@ -1,14 +1,14 @@
+import java.awt.Toolkit;
+
 import javax.swing.JButton;
-import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-import javax.swing.text.JTextComponent;
 
 public class UI {
 
-    public static void main() {
-        
+    public static void main(String[] args) {
+        new UI();
     }
 
     public UI() {
@@ -20,39 +20,35 @@ public class UI {
 
         x = 100;
         y = 100;
-        w = 100;
-        h = 100;
+        w = Toolkit.getDefaultToolkit().getScreenSize().width / 2;
+        h = Toolkit.getDefaultToolkit().getScreenSize().height / 2;
 
         JFrame interaction = new JFrame();
         interaction.setBounds(x, y, h, w);
+        interaction.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         interaction.setVisible(true);
 
-        interaction.add(setButton((int) 0.05*x, y, (int) 0.01*h, (int) 0.01*h));
-        interaction.add(setEditor((int) (x+(0.1*x)), (int) (y+(0.5*y)), (int) 0.1*h, (int) 0.9*w));
+        interaction.add(setButton((int) 0.05 * x, y, (int) 0.01 * h, (int) 0.01 * h));
+        interaction.add(setEditor((int) (x + (0.1 * x)), (int) (y + (0.5 * y)), (int) 0.1 * h, (int) 0.9 * w));
         interaction.add(setPasswordField(x, y, h, w));
-        
 
     }
 
-    public JButton setButton(int x, int y, int h, int w) {
+    public JButton setButton(int x, int y, int w, int h) {
         JButton accept = new JButton();
-        accept.setBounds(x,y,h,w);
-        accept.setVisible(true);
+        accept.setBounds(x, y, w, h);
         return accept;
     }
-    
 
-    public JTextField setEditor(int x, int y, int h, int w) {
+    public JTextField setEditor(int x, int y, int w, int h) {
         JTextField textField = new JTextField();
-        textField.setBounds(x,y,h,w);
-        textField.setVisible(true);
+        textField.setBounds(x, y, w, h);
         return textField;
     }
 
     public JPasswordField setPasswordField(int x, int y, int h, int w) {
         JPasswordField passwordField = new JPasswordField();
-        passwordField.setBounds(x,y,h,w);
-        passwordField.setVisible(true);
+        passwordField.setBounds(x, y, w, h);
         return passwordField;
     }
 }
