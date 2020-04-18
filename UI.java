aea7b021454e6921d22e3782cb707b8f84d11b2f
DerGhoul1 <56272358+DerGhoul1@users.noreply.github.com>
2020-04-18 23:50:18 +0200

    interaction ruft JComponents auf

--- UI.java
+++ UI.java
@@ -30,25 +30,29 @@ public class UI {
         interaction.add(setButton((int) 0.05*x, y, (int) 0.01*h, (int) 0.01*h));
         interaction.add(setEditor((int) (x+(0.1*x)), (int) (y+(0.5*y)), (int) 0.1*h, (int) 0.9*w));
         interaction.add(setPasswordField(x, y, h, w));
+        
 
     }
 
-    public void setButton(int x, int y, int h, int w) {
+    public JButton setButton(int x, int y, int h, int w) {
         JButton accept = new JButton();
         accept.setBounds(x,y,h,w);
         accept.setVisible(true);
+        return accept;
     }
+    
 
-    public void setEditor(int x, int y, int h, int w) {
+    public JTextField setEditor(int x, int y, int h, int w) {
         JTextField textField = new JTextField();
         textField.setBounds(x,y,h,w);
         textField.setVisible(true);
+        return textField;
     }
 
-    public void setPasswordField(int x, int y, int h, int w) {
+    public JPasswordField setPasswordField(int x, int y, int h, int w) {
         JPasswordField passwordField = new JPasswordField();
         passwordField.setBounds(x,y,h,w);
         passwordField.setVisible(true);
-
+        return passwordField;
     }
 }
