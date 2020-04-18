ec2380169329c446eebc815e004213e565ce506d
DerGhoul1 <56272358+DerGhoul1@users.noreply.github.com>
2020-04-19 00:03:29 +0200

    change screen

--- UI.java
+++ UI.java
@@ -7,7 +7,8 @@ import javax.swing.text.JTextComponent;
 
 public class UI {
 
-    public static void main() {
+    public static void main(String[] args) {
+        UI start = new UI();
         
     }
 
