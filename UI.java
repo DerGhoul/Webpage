b907096a6bab40496be06ac90cb61f6c2efc9fb6
DerGhoul1 <56272358+DerGhoul1@users.noreply.github.com>
2020-04-25 15:19:13 +0200

    Change JLabel with Java Editor

--- UI.java
+++ UI.java
@@ -1,69 +1,99 @@
-import java.awt.Toolkit;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+/**
+ *
+ * Beschreibung
+ *
+ * @version 1.0 vom 25.04.2020
+ * @author 
+ */
 
-public class UI {
+public class UI extends JFrame {
+  
+  
 
-    public static void main(String[] args) {
-        new UI();
-    }
+  // Anfang Attribute
+  private ImageIcon imageIcon1 = new ImageIcon(getClass().getResource("/Server2.png"));
+  private JLabel jLabel2 = new JLabel(imageIcon1);
+  private JLabel jLabel1 = new JLabel();
+  private JTextField jTextField1 = new JTextField();
+  private JButton jButton1 = new JButton();
+  private JButton jButton2 = new JButton();
+  private JPasswordField jPasswordField1 = new JPasswordField();
+  
+  // Ende Attribute
+  
+  
 
-    public UI() {
-        setFrame();
-    }
+  public UI() { 
+    // Frame-Initialisierung
+    super();
+    setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    int frameWidth = 868; 
+    int frameHeight = 614;
+    setSize(frameWidth, frameHeight);
+    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+    int x = (d.width - getSize().width) / 2;
+    int y = (d.height - getSize().height) / 2;
+    setLocation(x, y);
+    setTitle("UI");
+    setResizable(false);
+    Container cp = getContentPane();
+    cp.setLayout(null);
+    // Anfang Komponenten
+    
+    jLabel1.setBounds(96, 32, 137, 57);
+    jLabel1.setText("text");
+    cp.add(jLabel1);
+    jTextField1.setBounds(96, 200, 233, 49);
+    cp.add(jTextField1);
+    jButton1.setBounds(96, 368, 153, 73);
+    jButton1.setText("jButton1");
+    jButton1.setMargin(new Insets(2, 2, 2, 2));
+    jButton1.addActionListener(new ActionListener() { 
+      public void actionPerformed(ActionEvent evt) { 
+        jButton1_ActionPerformed(evt);
+      }
+    });
+    cp.add(jButton1);
+    jButton2.setBounds(632, 24, 153, 73);
+    jButton2.setText("jButton2");
+    jButton2.setMargin(new Insets(2, 2, 2, 2));
+    jButton2.addActionListener(new ActionListener() { 
+      public void actionPerformed(ActionEvent evt) { 
+        jButton2_ActionPerformed(evt);
+      }
+    });
+    
+    cp.add(jButton2);
+    jPasswordField1.setBounds(560, 200, 230, 44);
+    cp.add(jPasswordField1);
+    jLabel2.setBounds(8, 0, 849, 577);
+    jLabel2.setText("text");
+    cp.add(jLabel2);
+    // Ende Komponenten
+    
+    setVisible(true);
+  } // end of public UserUI
+  
+  // Anfang Methoden
+  
+  public static void main(String[] args) {
+    new UI();
+  } // end of main
+  
+  public void jButton1_ActionPerformed(ActionEvent evt) {
+    // TODO hier Quelltext einfügen
+    
+  } // end of jButton1_ActionPerformed
 
-    public void setFrame() {
-        int x, y, h, w;
+  public void jButton2_ActionPerformed(ActionEvent evt) {
+    // TODO hier Quelltext einfügen
+    
+  } // end of jButton2_ActionPerformed
 
-        x = 100;
-        y = 100;
-        w = Toolkit.getDefaultToolkit().getScreenSize().width / 2;
-        h = Toolkit.getDefaultToolkit().getScreenSize().height / 2;
-
-        JFrame interaction = new JFrame();
-        interaction.setBounds(x, y, w, h);
-        interaction.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        interaction.setLayout(null);
-        
-
-        interaction.add(setButton((int) (0.2 * w), (int) (0.7 * h), (int) (0.6 * w), (int) (0.1 * h)));
-        interaction.add(setServerLabel(x, y, h, w));
-        //interaction.add(setEditor((int) (x + (0.1 * x)), (int) (y + (0.5 * y)), (int) 0.1 * h, (int) 0.9 * w));
-        //interaction.add(setPasswordField(x, y, h, w));
-        
-        interaction.setVisible(true);
-    }
-
-    public JButton setButton(int x, int y, int w, int h) {
-        JButton accept = new JButton("ANMELDEN");
-        accept.setBounds(x, y, w, h);
-        return accept;
-    }
-
-    public JTextField setEditor(int x, int y, int w, int h) {
-        JTextField textField = new JTextField();
-        textField.setBounds(x, y, w, h);
-        return textField;
-    }
-
-    public JPasswordField setPasswordField(int x, int y, int h, int w) {
-        JPasswordField passwordField = new JPasswordField();
-        passwordField.setBounds(x, y, w, h);
-        return passwordField;
-    }
-
-    public JLabel setServerLabel(int x, int y, int h, int w) {
-        
-        ImageIcon i = new ImageIcon(getClass().getResource("/Server2.png"));
-        Object RenderingHints;
-	//	((Object) i).createScaledRendering(int, int, RenderingHints);
-        JLabel serverLabel = new JLabel(i);
-        serverLabel.setBounds(x, y, w, h);
-        return serverLabel;
-    }
-}
+  // Ende Methoden
+} // end of class UserUI
